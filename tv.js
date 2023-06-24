14d156a53a74682a2e3a99816bb9b4188980037b
TP1703 <85887100+Thanujaa6BC@users.noreply.github.com>
2023-06-24 20:55:58 +0530

    Add files via upload

---
+++ tv.js
@@ -0,0 +1,45 @@
+Status = "";
+TV_image = "";
+objects = [];
+
+function preload() {
+    TV_image = loadImage("TV.jpeg");
+}
+
+function setup() {
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_detector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("Status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
+    Status = true;
+    object_detector.Detect(TV_image, gotResults);
+
+}
+
+function gotResult(error, results) {
+    if (error) {
+      console.log(error);
+    }
+    console.log(results);
+    objects = results;
+  }
+
+function draw() {
+    image(TV_image,0,0);
+    if(Status != ""){
+        for(i=0; i< objects.length; i++){
+            document.getElementById("Status").innerHTML = "Status: Objects Detected";
+
+            fill("#FF0000");
+         percent = floor(objects[i].confidence * 100);
+         text(objects[i].label + " " + percent + "%", objects[i].x+15, objects[i].y+15);
+         noFill();
+         stroke("#FF0000");
+         rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
